75caa0c0e82291f7af99daf0be933412e243c582
Hassanbhatti <66300348+HassanrazaBhatti@users.noreply.github.com>
2022-02-02 05:23:43 -0800

    last touches

--- lib/page_one.dart
+++ lib/page_one.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 import 'package:hostel/page_two.dart';
